809a1719e0c1afc3a3db99b984bf1c6a4755f5ee
Nyashechka <mashanikolaeva2000@mail.ru>
2020-06-24 21:27:09 +0300

    end test

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,8 @@
 import GUI.*;
 
-import java.util.Vector;
-
 public class Main {
     public static void main(String[] args) {
         Visualizer visualizer = new Visualizer(500, 300);
         visualizer.setVisible(true);
-        int a = 5;
-        int b = 6;
-        string s = "Моя ветка)))))";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
     }
 }
