a38b348bf66d1a75300aee1cdacb755e19cdccc8
Nyashechka <mashanikolaeva2000@mail.ru>
2020-06-24 21:11:02 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Visualizer visualizer = new Visualizer(500, 300);
         visualizer.setVisible(true);
+        int a = 5;
+        int b = 6;
     }
 }
