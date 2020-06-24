d76a91544ede63e3d90c9f1f08838dca92a8cb07
olga.alexandrovnaa <melnikova.oa00@gmail.com>
2020-06-24 21:08:31 +0300

    hi

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Visualizer visualizer = new Visualizer(500, 300);
         visualizer.setVisible(true);
+        string s = "Hiiiiiiiiiii";
     }
 }
