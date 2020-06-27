9c8fd9228ec3bebd9390193269b5650fc0a03778
Alexander Shuka <shuka.sashaa@gmail.com>
2020-06-27 13:22:53 +0300

    add deleting and creating vertex when you press buttons

--- src/GUI/ListenableGraph.java
+++ src/GUI/ListenableGraph.java
@@ -5,25 +5,26 @@ import org.jgrapht.graph.SimpleDirectedGraph;
 
 
 public class ListenableGraph extends DefaultListenableGraph<Integer, EmptyEdge> {
-   ListenableGraph() {
-       super(new SimpleDirectedGraph<Integer, EmptyEdge>(EmptyEdge.class));
+    private int countOfVertex;
+   public ListenableGraph() {
+       super(new SimpleDirectedGraph<>(EmptyEdge.class));
+       countOfVertex = 0;
    }
 
-   public void testBuild() {
-       addVertex(1);
-       addVertex(2);
-       addVertex(3);
-       addVertex(4);
-       addVertex(5);
-       addVertex(6);
-       addVertex(7);
-       addVertex(8);
-       addVertex(9);
+   public void newVertex() {
+       addVertex(countOfVertex);
+       countOfVertex++;
+   }
 
-       addEdge(1, 3);
-       addEdge(5, 6);
-       addEdge(2, 9);
-       addEdge(1, 6);
-       addEdge(2, 5);
+   public void deleteVertex(int index) {
+       boolean isRemove = removeVertex(index);
+
+       if (isRemove) {
+           --countOfVertex;
+       }
+   }
+
+   public void deleteVertex() {
+       deleteVertex(countOfVertex - 1);
    }
 }
--- src/GUI/ScrollTextPane.java
+++ src/GUI/ScrollTextPane.java
@@ -3,19 +3,20 @@ package GUI;
 import javax.swing.*;
 
 public class ScrollTextPane extends JScrollPane {
-    private JTextArea textArea;
+    private final JTextArea textArea;
 
     ScrollTextPane() {
         textArea = new JTextArea();
         textArea.setLineWrap(true);
 
-        //FIX
-        textArea.setText("HELLO");
-
         setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         textArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         setViewportView(textArea);
+    }
+
+    public void updateText(String text) {
+        textArea.setText(text);
     }
 
     public JTextArea getTextArea() {
