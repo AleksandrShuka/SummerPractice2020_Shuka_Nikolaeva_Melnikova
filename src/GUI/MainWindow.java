fa5002084a1a37d12fe29da4a6883e44215967f3
Alexander Shuka <shuka.sashaa@gmail.com>
2020-07-01 22:25:22 +0300

    reworked GUI

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -15,8 +15,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -25,10 +23,12 @@ import java.util.logging.Level;
 
 
 public class MainWindow extends JFrame {
+    private boolean isPaused;
     private CommandPanel commandPanel;
     private ScrollTextPane scrollPane;
     private mxGraphComponent graphComponent;
     private MenuBar menuBar;
+    private Algorithm algo;
     private mxCircleLayout layout;
     private Graph graph;
 
@@ -42,10 +42,13 @@ public class MainWindow extends JFrame {
 
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 
+        int width = 2 * dimension.width / 3;
+        int height = 2 * dimension.height / 3;
+
         setBounds(dimension.width / 6, dimension.height / 6,
-                2 * dimension.width / 3, 2 * dimension.height / 3);
+                width, height);
         setResizable(false);
-        setLayout(new BorderLayout());
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 
         menuBar = new MenuBar();
         commandPanel = new CommandPanel();
@@ -53,27 +56,41 @@ public class MainWindow extends JFrame {
         graph = new Graph();
         layout = new mxCircleLayout(graph);
 
-        layout.setX0((double) dimension.width / 8);
-        layout.setY0(20.0);
-        layout.setRadius((double) dimension.height / 5);
+        layout.setX0(((double) width) / 20);
+        layout.setY0(((double) height) / 30);
 
         graphComponent = new mxGraphComponent(graph);
         graphComponent.setBackground(Color.LIGHT_GRAY);
-        graphComponent.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
+        graphComponent.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
         mxSwingConstants.VERTEX_SELECTION_COLOR = new Color(0xEEEEEE);
 
         graph.getSelectionModel().addListener(mxEvent.CHANGE, (o, mxEventObject) -> {
+            Logs.writeToLog(mxEventObject.getProperties().toString(), Level.INFO);
             ArrayList<mxCell> list = (ArrayList<mxCell>) mxEventObject.getProperty("added");
+            if (list != null) {
+                for (mxCell elem : list) {
+                    elem.setStyle(mxConstants.STYLE_FILLCOLOR + "=#FFFFFF");
+                }
+            }
 
-            if (list.size() == 0) {
-                list = (ArrayList<mxCell>) mxEventObject.getProperties().get("removed");
-                list.get(0).setStyle(mxConstants.STYLE_FILLCOLOR + "=" + "#00FF00");
-            } else {
-                list.get(0).setStyle(mxConstants.STYLE_FILLCOLOR + "=" + "#FFFFFF");
+            list = (ArrayList<mxCell>) mxEventObject.getProperties().get("removed");
+            if (list != null) {
+                for (mxCell elem : list) {
+                    elem.setStyle(mxConstants.STYLE_FILLCOLOR + "=#00FF00");
+                }
             }
 
             graph.refresh();
             graph.repaint();
+        });
+
+        commandPanel.getIncreaseSpeedButton().addActionListener(e -> algo.setSpeed(algo.getSpeed() - 50));
+
+        commandPanel.getDecreaseSpeedButton().addActionListener(e -> algo.setSpeed(algo.getSpeed() + 50));
+
+        commandPanel.getStopButton().addActionListener(e -> {
+            algo.cancel(true);
+            algo = new Algorithm(createGraph());
         });
 
         commandPanel.getAddVertexButton().addActionListener(e -> {
@@ -82,6 +99,11 @@ public class MainWindow extends JFrame {
         });
 
         commandPanel.getClearButton().addActionListener(e -> graph.clear());
+
+        commandPanel.getPauseButton().addActionListener(e -> {
+            algo.setRun(false);
+            isPaused = true;
+        });
 
         commandPanel.getDeleteButton().addActionListener(e -> {
             Object cell = graph.getSelectionCell();
@@ -95,21 +117,31 @@ public class MainWindow extends JFrame {
             executeGraph();
         });
 
+
         commandPanel.getStartButton().addActionListener(e -> {
-            Algorithm algo = new Algorithm(createGraph());
-            algo.addPropertyChangeListener(evt -> {
-                if (evt.getPropertyName().equals(Algorithm.TRANSPOSE_GRAPH)) {
-                    graph.transpose();
-                }
-            });
+            if (isPaused) {
+                algo.unSleep();
+            } else {
+                algo = new Algorithm(createGraph());
+                algo.addPropertyChangeListener(evt -> {
+                    if (evt.getPropertyName().equals(Algorithm.TRANSPOSE_GRAPH)) {
+                        graph.transpose();
+                    }
+                });
+            }
 
             algo.execute();
         });
 
         setJMenuBar(menuBar);
-        add(graphComponent, BorderLayout.CENTER);
-        add(commandPanel, BorderLayout.EAST);
-        add(scrollPane, BorderLayout.SOUTH);
+
+        scrollPane.setMaximumSize(new Dimension(30 * width / 100, height));
+        graphComponent.setMaximumSize(new Dimension(55 * width / 100, height));
+        commandPanel.setMaximumSize(new Dimension(15 * width / 100, height));
+
+        add(scrollPane);
+        add(graphComponent);
+        add(commandPanel);
     }
 
     private void executeGraph() {
