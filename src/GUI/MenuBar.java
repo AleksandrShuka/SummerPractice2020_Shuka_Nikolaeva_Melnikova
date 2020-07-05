44c03cd18610fa9e07eeea2e4f6b6ed8e670b7ce
Nyashechka <mashanikolaeva2000@mail.ru>
2020-07-05 12:56:20 +0300

    Added JavaDoc to MenuBar.java

--- src/GUI/MenuBar.java
+++ src/GUI/MenuBar.java
@@ -5,13 +5,28 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
-
+/**
+ * Класс, представляющий собой строку меню.
+ * Наследуется от {@code JMenuBar}.
+ *
+ * Содержит в себе экземпляры класса {@code JMenuItem}:
+ * @value open - кнопка для выбора файла для ввода графа из файла.
+ * @value exit - кнопка для выхода из приложения.
+ * @value help - кнопка для вызова справки.
+ * @value about - кнопка для вызова информации о программе.
+ *
+ * Содержит методы для создания меню File {@code createFileMenu} и меню Help {@code createHelpMenu}.
+ */
 public class MenuBar extends JMenuBar {
     private final JMenuItem open;
     private final JMenuItem exit;
     private final JMenuItem help;
     private final JMenuItem about;
 
+    /**
+     * Конструктор, устанавливающий фоновый цвет строке меню, инициализирующий поля класса и
+     * и добавляющий два экземпляра класса JMenu {@code add()} на строку меню.
+     */
     public MenuBar() {
         setBackground(Colors.getFirstBackgroundColor());
 
@@ -23,6 +38,7 @@ public class MenuBar extends JMenuBar {
         add(createFileMenu());
         add(createHelpMenu());
     }
+
 
     private @NotNull JMenu createFileMenu() {
         JMenu file = new JMenu("File");
