0c980003d33685bb789059aab903bbeb370363a7
Alexander Shuka <shuka.sashaa@gmail.com>
2020-07-05 14:35:14 +0300

    add JavaDoc Pair.java

--- src/GUI/Pair.java
+++ src/GUI/Pair.java
@@ -2,15 +2,27 @@ package GUI;
 
 import java.util.Objects;
 
+/**
+ * Класс-контейнер, представляющий собой пару объектов.
+ */
+
 public class Pair<T, U> {
     public final T first;
     public final U second;
 
+    /**
+     * Конструктор, принимающий два объекта {@code first} и {@code second}.
+     */
     public Pair(T first, U second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Перегруженный метод {@code equals}.
+     *
+     * @see Object#equals(Object)
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -20,6 +32,11 @@ public class Pair<T, U> {
                 Objects.equals(second, pair.second);
     }
 
+    /**
+     * Перегруженный метод {@code hashCode}.
+     *
+     * @see Object#hashCode()
+     */
     @Override
     public int hashCode() {
         return Objects.hash(first, second);
