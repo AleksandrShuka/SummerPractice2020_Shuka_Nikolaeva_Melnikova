02ea85d0a35aaf1399669d5f0499b04f2763b177
Alexander Shuka <shuka.sashaa@gmail.com>
2020-07-06 13:13:30 +0300

    add version 2.0

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -16,8 +16,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.*;
 
 /**
